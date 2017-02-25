f9b2bc32409c6b8ae9fe76bb9394c7d202645bd6
Vamsi Krishna B <vamsi@fastmail.com>
2017-02-25 17:40:38 +0000

    Apply fixes from StyleCI

--- src/Vamsi/ComposerTools/Command/DisplayTableCommand.php
+++ src/Vamsi/ComposerTools/Command/DisplayTableCommand.php
@@ -3,21 +3,20 @@
 namespace Vamsi\ComposerTools\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Vamsi\ComposerTools\Info;
-use Symfony\Component\Console\Helper\Table;
 
 class DisplayTableCommand extends Command
 {
     protected function configure()
     {
         $this
-            ->setName("display")
-            ->setDescription("Displays the extracted info")
+            ->setName('display')
+            ->setDescription('Displays the extracted info')
             ->addArgument('path', InputArgument::REQUIRED, 'The directory where composer.lock can be found');
-
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -27,13 +26,13 @@ class DisplayTableCommand extends Command
         $packages = $info->getPackages();
         $tdata = [];
         foreach ($packages as $package) {
-            $tdata[] = array($package->getName(), $package->getTime()->format('Y-m-d H:i:s'));
+            $tdata[] = [$package->getName(), $package->getTime()->format('Y-m-d H:i:s')];
         }
 
         $table = new Table($output);
 
         $table
-            ->setHeaders(array('Name', 'Release Time'))
+            ->setHeaders(['Name', 'Release Time'])
             ->setRows($tdata);
 
         $table->render();
